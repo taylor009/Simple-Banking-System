87cfccdf0fbc6ab8cb3dcf0f60b623c24a146e5f
Taylor Gagne <tgagne@netsurion.com>
2019-11-04 10:42:43 -0500

    First commit

---
+++ cmake-build-debug/CMakeFiles/bank_system.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/bank_system.dir/main.cpp.o"
+  "bank_system"
+  "bank_system.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/bank_system.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
